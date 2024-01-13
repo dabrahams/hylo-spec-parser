b8c796cdd1287a24214aabe0596b4c46ff88f8de
Dave Abrahams <dabrahams@adobe.com>
2024-01-12 18:35:36 -0800

    Delete everything that depends on the spec.

--- Sources/ParseGen/Spec.swift
+++
@@ -1,4 +0,0 @@
-/// The path to the specification file
-public let specPath
-  = "/" + (#filePath.split(separator: "/").dropLast(3)
-             + ["spec", "spec.md"]).joined(separator: "/")
--- Sources/Utils/String+Extensions.swift
+++ Sources/Utils/String+Extensions.swift
@@ -2,6 +2,7 @@ import Foundation
 import LoftDataStructures_Zip2Collection
 
 extension StringProtocol {
+
   /// Returns the slice of self that remains after dropping leading and trailing whitespace.
   public func strippingWhitespace() -> SubSequence
     where Element == Character
@@ -10,17 +11,4 @@ extension StringProtocol {
       .dropLast { c in c.isWhitespace }
   }
 
-  /// Returns the lines from markdown fenced code blocks in `self` (having the
-  /// given language if `language` is non-`nil`), paired with their line numbers.
-  public func markdownCodeBlocks(language: String? = nil)
-    -> [Zip2Collection<Range<Int>, [Self.SubSequence]>.SubSequence]
-  {
-    let allLines = self.split(omittingEmptySubsequences: false) { c in c.isNewline }.enumerated()
-    let fence = "```" + (language ?? "")
-    return allLines.subSequencesBetween(
-      elementsSatisfying: { l in l.1.strippingWhitespace() == fence },
-      and: { l in l.1.strippingWhitespace() == fence.prefix(3) }
-    )
-  }
 }
-
